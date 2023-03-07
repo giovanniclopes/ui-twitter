e31ae4acee88225ee98518fc333c25a59765e660
Giovanni Lopes <giovanniclopes@gmail.com>
2023-03-07 19:55:16 -0300

    app layout created

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { House } from "phosphor-react";
+import {
+  Bell,
+  BookmarkSimple,
+  DotsThreeCircle,
+  Envelope,
+  FileText,
+  Hash,
+  House,
+  User,
+} from "phosphor-react";
 
 import "./global.css";
 
@@ -13,17 +22,38 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <img className="logo" src={twitterLogo} alt="Logo" />
 
         <nav className="main-navigation">
-          <a href="">
-            <House />
+          <a className="active" href="">
+            <House weight="fill" />
             Home
           </a>
-          <a href="">Explore</a>
-          <a href="">Notifications</a>
-          <a href="">Messages</a>
-          <a href="">Bookmarks</a>
-          <a href="">Lists</a>
-          <a href="">Profile</a>
-          <a href="">More</a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
         </nav>
 
         <button className="new-tweet" type="button">
