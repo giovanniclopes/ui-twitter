c0e9b4733cef4fe49c2e4d4b21182ef0b2642fb7
Giovanni Lopes <giovanniclopes@gmail.com>
2023-03-07 21:14:59 -0300

    routes created

--- src/main.tsx
+++ src/main.tsx
@@ -1,41 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { Sidebar } from "./components/Sidebar";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes";
 import "./global.css";
 
-import { Tweet } from "./components/Tweet";
-import { Sidebar } from "./components/Sidebar";
-import { Header } from "./components/Header";
-import { Separator } from "./components/Separator";
-
-const tweets = ["Meu primeiro tweet", "Teste", "Deu certo tweetar!"];
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
       <Sidebar />
+      
       <div className="content">
-        <main className="timeline">
-          <Header title="Home" />
-
-          <form className="new-tweet-form">
-            <label htmlFor="tweet">
-              <img
-                src="https://github.com/giovanniclopes.png"
-                alt="Giovanni Lopes"
-              />
-              <textarea id="tweet" placeholder="What's happening?" />
-            </label>
-
-            <button type="submit">Tweet</button>
-          </form>
-
-          <Separator />
-
-          {tweets.map((tweet) => {
-            return <Tweet key={tweet} content={tweet} />;
-          })}
-        </main>
+        <RouterProvider router={router} />
       </div>
     </div>
   </React.StrictMode>
