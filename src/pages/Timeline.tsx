55808d07f525586351218a630f52893bd263cd69
Giovanni Lopes <giovanniclopes@gmail.com>
2023-03-07 23:51:32 -0300

    Update Timeline.tsx

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,23 +1,45 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 
-import './Timeline.css'
+import "./Timeline.css";
 
-const tweets = ["Meu primeiro tweet", "Teste", "Deu certo tweetar!"];
+let newTweet = "";
 
 export function TimeLine() {
+  const [newTweet, setNewTweet] = useState('')
+  const [tweets, setTweets] = useState([
+    "Meu primeiro tweet",
+    "Teste",
+    "Deu certo tweetar!",
+  ]);
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+
+    setTweets([newTweet, ...tweets]);
+    setNewTweet('')
+  }
+
   return (
     <main className="timeline">
       <Header title="Home" />
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img
             src="https://github.com/giovanniclopes.png"
             alt="Giovanni Lopes"
           />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            value={newTweet}
+            onChange={(event) => {
+              setNewTweet(event.target.value);
+            }}
+          />
         </label>
 
         <button type="submit">Tweet</button>
